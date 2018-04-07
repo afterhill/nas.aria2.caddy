1cd8759e5d22912da9c0e6b205493931e93552d8
Meng Jue <mengjue@outlook.com>
2018-04-08 03:56:34 +0800

    Update get-bt-list.sh

--- conf/get-bt-list.sh
+++ conf/get-bt-list.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-killall aria2c
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [ -z "`grep "bt-tracker" /root/conf/aria2.conf`" ]; then
     sed -i '$a bt-tracker='${list} /root/conf/aria2.conf

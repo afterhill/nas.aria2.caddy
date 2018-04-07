cab169a114916adc987bc998fe4d7a96d4377236
Meng Jue <mengjue@outlook.com>
2018-04-08 02:43:16 +0800

    Update get-bt-list.sh

--- conf/get-bt-list.sh
+++ conf/get-bt-list.sh
@@ -2,9 +2,9 @@
 killall aria2c
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [ -z "`grep "bt-tracker" /root/conf/aria2.conf`" ]; then
-    sed -i '$a bt-tracker='${list} /root/.aria2/aria2.conf
-    echo add......
+    sed -i '$a bt-tracker='${list} /root/conf/aria2.conf
+    echo "add bt tracker list" > /proc/1/fd/1 2>/proc/1/fd/2
 else
-    sed -i "s@bt-tracker.*@bt-tracker=$list@g" /root/.aria2/aria2.conf
-    echo update......
+    sed -i "s@bt-tracker.*@bt-tracker=$list@g" /root/conf/aria2.conf
+    echo "update bt tracker list" > /proc/1/fd/1 2>/proc/1/fd/2
 fi

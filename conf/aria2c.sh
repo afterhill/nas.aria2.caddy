be313c982dc3ed707ca9abd5b2c2b955e38d97b3
Meng Jue <mengjue@outlook.com>
2018-04-08 02:40:48 +0800

    Update aria2c.sh

--- conf/aria2c.sh
+++ conf/aria2c.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 echo "Run aria2c and ariaNG"
 if [ "$ENABLE_AUTH" = "true" ]; then
-  echo "Using Basic Auth config file "
+  echo "Using Basic Auth config file " > /proc/1/fd/1 2>/proc/1/fd/2
   CADDY_FILE=/usr/local/caddy/SecureCaddyfile
 else
-  echo "Using caddy without Basic Auth"
+  echo "Using caddy without Basic Auth" > /proc/1/fd/1 2>/proc/1/fd/2
   CADDY_FILE=/usr/local/caddy/Caddyfile
 fi
 
 if [ "$SSL" = "true" ]; then
-echo "Start aria2 with secure config"
+echo "Start aria2 with secure config" > /proc/1/fd/1 2>/proc/1/fd/2
 
 /usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D  \
 --enable-rpc --rpc-listen-all  \
@@ -19,7 +19,7 @@ echo "Start aria2 with secure config"
 && caddy -quic --conf ${CADDY_FILE}
 
 else
-echo "Start aria2 with standard mode"
+echo "Start aria2 with standard mode" > /proc/1/fd/1 2>/proc/1/fd/2
 /usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D \
 --enable-rpc --rpc-listen-all \
 && caddy -quic --conf ${CADDY_FILE}
